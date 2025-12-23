f3e2c6ca64c96ad21c1a27482892c02d2e0ad0db
Gabriel Lima <gabrieeelima15@gmail.com>
2025-12-22 22:43:12 -0300

    add h1, h2 and h2 in the MainView

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -2,16 +2,17 @@ package com.example;
 
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-/* The @Route annotation turns this class into a view, mapping it to the root URL ("/") of the application. */
-@Route("")
-@PageTitle(value = "Hello, Vaadin!!!!")
-public class MainView extends VerticalLayout{
+@Route("") // The @Route annotation turns this class into a view, mapping it to the root URL ("/") of the application.
+@PageTitle(value = "Hello, Vaadin!!!!") //The @PageTitle annotation, is for sets page title of web page
+public class MainView extends VerticalLayout{ // MainView extends VerticalLayout (it's layout component) for children vertically
     public MainView() {
-        add(new H1("Hello, Vaadin!"));
+        add(new H1("Hello, Vaadin!")); //H1 and H2 is a componente that corresponds to the HTML (a top-level heading)
         add(new H2("Isso é satisfatório."));
+        add(new H3("It's cool"));
     }
 }
