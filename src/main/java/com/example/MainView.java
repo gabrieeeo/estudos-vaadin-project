2865682b00876ad56eb1892759df7244f77f1181
Gabriel Lima <gabrieeelima15@gmail.com>
2025-12-22 23:05:17 -0300

    add button Say Hello

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,5 +16,6 @@ public class MainView extends VerticalLayout{ // MainView extends VerticalLayout
         add(new H1("Hello, Vaadin!")); //H1 and H2 is a componente that corresponds to the HTML (a top-level heading)
         add(new H2("Isso é satisfatório."));
         add(new H3("It's cool"));
+        add(new Button("Say Hello", event -> add(new Span("Hello, it's Vaadin broooo"))));
     }
 }
